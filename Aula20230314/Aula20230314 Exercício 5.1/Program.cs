0cbed2b9e34a10cf80e1373ecaeccc27eb47ffe5
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-03-16 15:17:05 -0400

    Alteração do IMC do 5.1 e adicionado 5.2 (sendo esta, incompleta)

--- Aula20230314/Aula20230314 Exercício 5.1/Program.cs
+++ Aula20230314/Aula20230314 Exercício 5.1/Program.cs
@@ -12,7 +12,7 @@ namespace Aula20230314_Exercício_2._1
         {
             Console.WriteLine("\t\t---Exercício 5.1---");
 
-            Console.WriteLine("Calculador de IMC");
+            Console.WriteLine("Calculador de IMC\n");
             Console.Write("Digite seu peso: ");
             double peso = double.Parse(Console.ReadLine());
             Console.Write("Digite sua altura: ");
@@ -20,35 +20,43 @@ namespace Aula20230314_Exercício_2._1
             double imc = peso / (altura * altura);
             if (imc < 16.00)
             {
-                Console.WriteLine("Baixo peso grau III");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: baixo peso grau III");
             }
             if (imc >= 16.00 && imc <= 16.99)
             {
-                Console.WriteLine("Baixo peso grau II");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: baixo peso grau II");
             }
             if (imc >= 17.00 && imc <= 18.49)
             {
-                Console.WriteLine("Baixo peso grau I");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: baixo peso grau I");
             }
             if (imc >= 18.50 && imc <= 24.99)
             {
-                Console.WriteLine("Peso ideal");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: peso ideal");
             }
             if (imc >= 25.00 && imc <= 29.99)
             {
-                Console.WriteLine("Sobrepeso");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: sobrepeso");
             }
             if (imc >= 30.00 && imc <= 34.99)
             {
-                Console.WriteLine("Obesidade grau I");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: obesidade grau I");
             }
             if (imc >= 35.00 && imc <= 39.99)
             {
-                Console.WriteLine("Obesidade grau II");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: obesidade grau II");
             }
             if (imc >= 40.00)
             {
-                Console.WriteLine("Obesidade grau III");
+                Console.WriteLine("\nSeu IMC: {0}", imc);
+                Console.WriteLine("Classificação: obesidade grau III");
             }
 
             Console.ReadLine();
--- Aula20230314/Aula20230314 Exercício 5.2/Program.cs
+++ Aula20230314/Aula20230314 Exercício 5.2/Program.cs
@@ -12,7 +12,24 @@ namespace Aula20230314_Exercício_5._2
         {
             Console.WriteLine("\t\t---Exercício 5.2---");
 
+            Console.WriteLine("Digite o primeiro lado: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo lado: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro lado: ");
+            double c = double.Parse(Console.ReadLine());
 
+            if ((b-a)<a && a<(b+c) || (a-c) < b && b < (a+c) || (a-b) < c && c > (b+c))
+            {
+                if ((a == b) && (b == c))
+                {
+                    Console.WriteLine("Triângulo Equilátero");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
 
             Console.ReadLine();
         }
