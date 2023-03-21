d66f7afeaeb3ad7787579733c742516e0b35d70b
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-03-21 11:44:07 -0400

    Exercício 1 e 2 de Switch Case adicionado

---
+++ Aula20230321 Exercícios (Switch Case)/Aula20230321 Exercício 2 (Switch Case)/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230321_Exercício_2__Switch_Case_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 2---");
+
+            Console.WriteLine("\nDigite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a nota do 1º Bimestre: ");
+            int nota1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do 2º Bimestre: ");
+            int nota2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do 3º Bimestre: ");
+            int nota3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do 4º Bimestre: ");
+            int nota4 = int.Parse(Console.ReadLine());
+            int notaMedia = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            char Conceito = ' ';
+
+            if (notaMedia > 100)
+            {
+                Console.WriteLine("Erro na média da nota: está ultrapassado!");
+            }
+            else if (notaMedia >= 90)
+            {
+                Conceito = 'A';
+            } 
+            else if (notaMedia >= 80)
+            {
+                Conceito = 'B';
+            }
+            else if (notaMedia >= 60)
+            {
+                Conceito = 'C';
+            }
+            else if (notaMedia >= 40)
+            {
+                Conceito = 'D';
+            }
+            else if (notaMedia >= 20)
+            {
+                Conceito = 'E';
+            }
+            else if (notaMedia < 20)
+            {
+                Conceito = 'F';
+            }
+            else if (notaMedia < 0)
+            {
+                Console.WriteLine("Erro na média da nota: está negativo!");
+            }
+            switch (Conceito)
+            {
+                case 'A':
+                    Console.WriteLine("Olá {0}! Sua nota é {1} e você está aprovado", nome, Conceito);
+                    break;
+                case 'B':
+                    Console.WriteLine("Olá {0}! Sua nota é {1} e você está aprovado", nome, Conceito);
+                    break;
+                case 'C':
+                    Console.WriteLine("Olá {0}! Sua nota é {1} e você está aprovado", nome, Conceito);
+                    break;
+                case 'D':
+                    Console.WriteLine("Olá {0}! Sua nota é {1} e você está de recuperação", nome, Conceito);
+                    break;
+                case 'E':
+                    Console.WriteLine("Olá {0}! Sua nota é {1} e você está de exame", nome, Conceito);
+                    break;
+                case 'F':
+                    Console.WriteLine("Olá {0}! Sua nota é {1} e você está reprovado", nome, Conceito);
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula20230321 Exercícios (Switch Case)/Aula20230321 Exercícios (Switch Case)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230321_Exercícios__Switch_Case_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 1---");
+
+            Console.WriteLine("\nCargos: \n\t[1] Gerente \n\t[2] Engenheiro \n\t[3] Técnico \n\t[4] Outro");
+            Console.Write("\nInforme o cargo: ");
+            int cargo = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme o seu salário: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            double novosal = 0;
+            double reajuste = 0;
+
+            switch (cargo)
+            {
+                case 1:
+                    novosal = salario + (salario * 0.1);
+                    reajuste = novosal - salario;
+                    break;
+                case 2:
+                    novosal = salario + (salario * 0.2);
+                    reajuste = novosal - salario;
+                    break;
+                case 3:
+                    novosal = salario + (salario * 0.3);
+                    reajuste = novosal - salario;
+                    break;
+                case 4:
+                    novosal = salario + (salario * 0.4);
+                    reajuste = novosal - salario;
+                    break;
+                default:
+                    novosal = salario;
+                    break;
+            }
+            Console.WriteLine("\nSalário antigo: {0:c2} \nSalário novo: {1:c2} \nReajuste: {2:c2}", salario, novosal, reajuste);
+
+            Console.ReadLine();
+        }
+    }
+}
