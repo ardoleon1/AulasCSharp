ba028ea65f68b1ce17db627de0e93dda8fdff027
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-04-11 11:32:15 -0400

    Adicionado Aula20230411 Exercícios 1 e 2

---
+++ Aula20230411 Exercícios While/Aula20230411 Exercício 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230411_Exercício_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 2---");
+            Console.Write("Digite a quantidade de discentes: ");
+            int discente = int.Parse(Console.ReadLine());
+            int nota1, nota2, nota3, nota4, notaMedia, notaTurma = 0, notaTotal, discenteFinal;
+            discenteFinal = discente;
+            Console.Clear();
+            while (discenteFinal != 0)
+            {
+                Console.WriteLine("\t\t---Exercício 2---");
+                Console.Write("Digite a nota do 1º Bimestre: ");
+                nota1 = int.Parse(Console.ReadLine());
+                Console.Write("Digite a nota do 2º Bimestre: ");
+                nota2 = int.Parse(Console.ReadLine());
+                Console.Write("Digite a nota do 3º Bimestre: ");
+                nota3 = int.Parse(Console.ReadLine());
+                Console.Write("Digite a nota do 4º Bimestre: ");
+                nota4 = int.Parse(Console.ReadLine());
+                notaMedia = (nota1 + nota2 + nota3 + nota4) / 4;
+                Console.WriteLine("A média deste discente é {0}", notaMedia);
+                notaTurma = notaTurma + notaMedia;
+                discenteFinal--;
+            }
+            notaTotal = notaTurma / discente;
+            Console.WriteLine("A média da turma é {0}", notaTotal);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula20230411 Exercícios While/Aula20230411 Exercícios While/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230411_Exercícios_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 1;
+            while (op == 1)
+            {
+                Console.WriteLine("\t\t---Exercício 1---");
+                Console.Write("Informe um número: ");
+                int num = int.Parse(Console.ReadLine());
+
+                int cont = 0;
+                while (cont <= 10)
+                {
+                    Console.WriteLine($"{num} x {cont} = {num * cont}");
+                    cont++;
+                }
+                Console.WriteLine("\nDeseja realizar outra operação?\n[0]Não\n[1]Sim");
+                Console.Write("Escolha uma opção: ");
+                op = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
