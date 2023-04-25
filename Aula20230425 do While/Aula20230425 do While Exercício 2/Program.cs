88837b811b58f50deb5a46fc3f3ee7fd61696651
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-04-25 11:42:59 -0400

    Exercícios 1 e 2 do Do While adicionado

---
+++ Aula20230425 do While/Aula20230425 do While Exercício 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230425_do_While_Exercício_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nvcand1 = 0, nvcand2 = 0, nvcand3 = 0, nvbrancos = 0, nvnulos = 0, neleitores = 0, op;
+            string resposta = "SIM";
+            do
+            {
+                Console.WriteLine("Data: "+DateTime.Now.ToString("dd-mm-aaaa"));
+
+                Console.WriteLine("\n\t\t---Qual é a melhor série de todos os tempos?");
+                Console.WriteLine("\n\t[1]The Big Bang Theory \n\t[2]Game of Thrones\n\t[3]The Glory\n\t[4]Outras\n\tNão sei!");
+                Console.WriteLine("\nInforme a sua opção: ");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        nvcand1++;
+                        break;
+                    case 2:
+                        nvcand2++;
+                        break;
+                    case 3:
+                        nvcand3++;
+                        break;
+                    case 4:
+                        nvbrancos++;
+                        break;
+                    case 5:
+                        nvnulos++;
+                        break;
+                }
+                Console.WriteLine("\nVoto realizado com sucesso!");
+                Console.WriteLine("\nDeseja registrar outro voto?");
+                Console.WriteLine("Digite [S] ou [SIM] para prosseguir: ");
+                resposta = Console.ReadLine().ToUpper();
+
+            } while (resposta == "SIM" || resposta == "S");
+
+            neleitores = nvcand1 + nvcand2 + nvcand3 + nvbrancos + nvnulos;
+            Console.WriteLine("\n\t\t---Resultado da Votação---");
+            Console.WriteLine("\nNúmero de votos: "+neleitores);
+            Console.WriteLine("The Big Bang Theory: "+nvcand1);
+            Console.WriteLine("Game of Thrones: "+nvcand2);
+            Console.WriteLine("The Glory: "+nvcand3);
+            Console.WriteLine("Outras: "+nvbrancos);
+            Console.WriteLine("Não sei/Nulo: "+nvnulos);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Aula20230425 do While/Aula20230425 do While/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230425_do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Ajuda nas Compras---");
+
+            Console.Write("Informe o valor disponível: ");
+            double saldo = double.Parse(Console.ReadLine());
+            double compra = 0;
+            double somaCompras = 0;
+            int cont = 0;
+            bool podeComprar = true;
+
+            do
+            {
+                Console.Write("\nValor da compra: ");
+                compra = double.Parse(Console.ReadLine());
+                if (compra > 0)
+                {
+                    if (compra <= saldo)
+                    {
+                        saldo = saldo - compra;
+                        somaCompras = somaCompras + compra;
+                        cont++;
+                        Console.WriteLine("\nCompra registrada!\nSaldo disponível: {0:c2}", saldo);
+                    }
+                    else
+                    {
+                        podeComprar = false;
+                        Console.WriteLine("Você não possui saldo suficiente para realizar essa compra");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nValor inválido!");
+                }
+
+            } while (podeComprar);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("\t\t---Resultado das Compras---");
+            Console.WriteLine("\nNúmero de Compras: {0}\nTotal Gasto: {1:c2}\nSaldo Restante: {2:c2}", cont, somaCompras, saldo);
+            Console.ReadKey();
+        }
+    }
+}
