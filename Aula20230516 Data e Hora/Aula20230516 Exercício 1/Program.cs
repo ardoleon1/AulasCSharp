9b82b08871fb0691675757436651ae6079e53f58
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-16 11:44:12 -0400

    Exercício 1 feito mas exercício 2 incompleto

---
+++ Aula20230516 Data e Hora/Aula20230516 Exercício 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230516_Exercício_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 1---");
+            DateTime dtAtual = DateTime.Now;
+            Console.WriteLine(dtAtual);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula20230516 Data e Hora/Aula20230516 Exercício 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230516_Exercício_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 2---");
+            Console.Write("\nDigite a sua data de Nascimento (Obs.: no formato dd/mm/yyyy): ");
+            DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+            if ()
+
+            Console.ReadLine();
+        }
+    }
+}
