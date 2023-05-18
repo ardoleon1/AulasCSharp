65df2534560c57d3f6c24b66924a5f70cc845fe7
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-18 15:09:24 -0400

    Exercício 3 adicionado e corrigido com método

---
+++ Aula20230516 Data e Hora/Aula20230516 Exercício 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230516_Exercício_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 3---");
+
+            Console.Write("Informe a data de nascimento (Obs.: no formato dd/mm/yyyy): ");
+            DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+            CalcularIdadeDetalhada(dtNascimento);
+            Console.ReadKey();
+        }
+        static void CalcularIdadeDetalhada(DateTime dtNascimento)
+        {
+            DateTime dtAtual = DateTime.Now;
+            TimeSpan diferenca = dtAtual - dtNascimento;
+            DateTime idade = DateTime.MinValue + diferenca;
+
+            int anos = idade.Year - 1;
+            int meses = idade.Month - 1;
+            int dias = idade.Day - 1;
+
+            Console.WriteLine("{0} Anos\n{1} Meses\n{2} Dias", anos, meses, dias);
+        }
+    }
+}
