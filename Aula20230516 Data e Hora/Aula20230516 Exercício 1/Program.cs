7f9b741020b37e948d25830a4709e839e7f7c04a
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-18 14:13:38 -0400

    Exercício 1 corrigido

--- Aula20230516 Data e Hora/Aula20230516 Exercício 1/Program.cs
+++ Aula20230516 Data e Hora/Aula20230516 Exercício 1/Program.cs
@@ -12,7 +12,10 @@ namespace Aula20230516_Exercício_1
         {
             Console.WriteLine("\t\t---Exercício 1---");
             DateTime dtAtual = DateTime.Now;
-            Console.WriteLine(dtAtual);
+            string data = dtAtual.ToString("dd/MM/yyyy");
+            string hora = dtAtual.ToString("HH:mm");
+            Console.WriteLine("Data: " + data);
+            Console.WriteLine("Hora: " + hora);
 
             Console.ReadLine();
         }
