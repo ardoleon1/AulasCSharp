e75b0e9ca7f1d28595cc9e7bb3052afed452faec
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-25 14:51:58 -0400

    Exercício 2 feito

---
+++ Aula20230525 Array/Aula20230525 Exercício 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230525_Exercício_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 2---");
+            int[] num = new int[101];
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = i;
+            }
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
