337cf2352a81cdd2f6611f29d252dc1bfa6a882e
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-25 15:05:20 -0400

    Exercício 3 adicionado

---
+++ Aula20230525 Array/Aula20230525 Exercício 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230525_Exercício_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 3---");
+            char[] alfabeto = new char[26];
+            for (int i = 0; i < alfabeto.Length; i++)
+            {
+                alfabeto[i] = (char)('A' + i);
+            }
+            foreach (char letra in alfabeto)
+            {
+                Console.WriteLine(letra);
+            }
+            Console.ReadLine();
+        }
+    }
+}
