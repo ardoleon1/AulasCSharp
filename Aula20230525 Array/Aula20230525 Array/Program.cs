82a113b426a73044e4d7986e0aeb25cf1f76a676
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-25 14:47:14 -0400

    Exercício 1 feito

---
+++ Aula20230525 Array/Aula20230525 Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230525_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 1---");
+
+            string[] nomes = new string[5];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write("Informe o {0}º nome: ", i+1);
+                nomes[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\n\t\t---Imprimindo os dados---");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+            //for (int i = 0; i < nomes.Length; i++)
+            //{
+            //    Console.WriteLine(nomes[i]);
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
