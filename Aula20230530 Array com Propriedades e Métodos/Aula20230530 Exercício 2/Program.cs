dad6a7958e0b0c4899592ffda46855cca500da5d
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-06-01 14:22:38 -0400

    Exercício 2 adicionado e outras correções

---
+++ Aula20230530 Array com Propriedades e Métodos/Aula20230530 Exercício 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20230530_Exercício_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 2---");
+
+            int[] x = new int[10];
+            int i = 0;
+
+            while(i < x.Length)
+            {
+                Console.Write("Informe o {0}º valor: ", i + 1);
+                int valor = int.Parse(Console.ReadLine());
+
+                x[i] = valor;
+                i++;
+            }
+            int[] y = (int[]) x.Clone();
+            Array.Reverse(y);
+            Console.WriteLine("\n\t\t---Imprimindo os valores na tela---");
+            foreach (var item in y)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
